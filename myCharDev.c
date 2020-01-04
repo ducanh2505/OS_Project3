adc6b68495aee136a371c1e8875780554198d77d
root <ducanh250599@gmail.com>
2020-01-04 14:48:43 +0700

    simple read and write method

--- myCharDev.c
+++ myCharDev.c
@@ -6,6 +6,7 @@
 #include <linux/device.h>
 #include <linux/err.h>
 #include <linux/uaccess.h>
+#include <linux/slab.h>
 
 #define MINOR_FIRST 0
 #define MINOR_COUNT 1
@@ -17,7 +18,7 @@ static struct class *my_class;
 static struct device *my_device;
 static size_t size_of_buff=0;
 static size_t my_offset = 0;
-char my_buff[BUFFER_SIZE]={0};
+char *my_buff = NULL;
 
 static dev_t my_device_number;
 
@@ -37,18 +38,38 @@ struct file_operations my_fops=
 
 static ssize_t my_read(struct file *filp, char __user *buffer, size_t count, loff_t *offset)
 {
+	printk(KERN_INFO"My Chrdev: read method %s size : %zu count :%zu ",my_buff,size_of_buff,count);
 	int err_count = 0; 
-    if (size_of_buff - offset >= count)
-	    err_count = copy_to_user(buffer, my_buff, size_of_buff);
-    else
-    {
-        printk(KERN_INFO "My Chrdev: Error lenght");
-        return -EFAULT;
-    }
+	if ((int)size_of_buff == 0)
+	{
+		printk(KERN_INFO"My Chrdev: Buffer is empty");
+		return 0;
+	}
+
+    if (count > size_of_buff ) 
+	{
+		err_count = copy_to_user(buffer, my_buff, size_of_buff);
+		my_buff =(char*) memset(my_buff,0,BUFFER_SIZE);
+		
+	}
+	else	 if (size_of_buff >= count)
+		{
+			
+			err_count = copy_to_user(buffer, my_buff, count);
+			char *t = my_buff + count;
+			my_buff = (char*) strcpy(my_buff,t);
+			
+		}
+		else
+		{
+			printk(KERN_INFO "My Chrdev: Error lenght size : %zu offset : %zu  count :%zu ",size_of_buff,my_offset,count);
+			return -EFAULT;
+		}
+
 	if( err_count == 0 )
 	{
-		printk(KERN_INFO "My Chrdev: Sent string %s to the user\n",my_buff);
-		my_offset += count;
+		size_of_buff = strlen(my_buff);
+		printk(KERN_INFO "My Chrdev: Sent string \"%s\" to the user\n",my_buff);
 		return 0;
 	}else
 	{
@@ -60,24 +81,27 @@ static ssize_t my_read(struct file *filp, char __user *buffer, size_t count, lof
 static ssize_t my_write(struct file *filp, const char __user *buffer, size_t count, loff_t *offset)
 {
     int i;
-   
+   printk(KERN_INFO"My Chrdev: write method");
 	if(copy_from_user(my_buff + size_of_buff, buffer, count))
 	{
 		return -EACCES;
 	}
 
-	size_of_buff = strlen(buff);
+	size_of_buff = strlen(my_buff);
 	printk(KERN_INFO "My Chrdev: receive %zu charaters for the user %s\n",count,my_buff);
 	return count;
 }
 
 static int my_open(struct inode *node, struct file *filp)
 {
+	
+	
 	return 0;
 }
 
 static int my_release(struct inode *node, struct file *filp)
 {
+	
 	return 0;
 }
 
@@ -87,6 +111,9 @@ void __exit my_exit(void)
 	class_destroy(my_class);
 	cdev_del(&my_cdev);
 	unregister_chrdev_region(my_device_number, MINOR_COUNT);
+	kfree(my_buff);
+	
+	printk(KERN_INFO "My Chrdev: Unload succesfull\n");
 }
 
 int __init my_init(void)
@@ -125,6 +152,9 @@ int __init my_init(void)
 		printk(KERN_WARNING "Cannot create device file");
 		return PTR_ERR(my_device);
 	}
+	my_buff = (char*) kmalloc(BUFFER_SIZE,GFP_KERNEL);
+	my_buff =(char*) memset(my_buff,0,BUFFER_SIZE);
+	printk(KERN_INFO "My Chrdev: Init succesfull\n");
 	return 0;
 }
 
